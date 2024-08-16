673c3ee1797a00ecc4341e73da29b8e06848565a
r22dd <david.dams@tutanota.com>
2024-08-16 14:29:28 +0200

    FEAT(chiral sensor): lrt

---
+++ chiral_sensor/sim_lr.py
@@ -0,0 +1,143 @@
+from itertools import combinations
+
+import matplotlib.pyplot as plt
+import jax.numpy as jnp
+
+from granad import *
+
+def get_haldane_graphene(t1, t2, delta):
+    """Constructs a graphene model with
+    onsite hopping difference between sublattice A and B, nn hopping, nnn hopping = delta, t1, t2
+    """
+    return (
+        Material("haldane_graphene")
+        .lattice_constant(2.46)
+        .lattice_basis([
+            [1, 0, 0],
+            [-0.5, jnp.sqrt(3)/2, 0]
+        ])
+        .add_orbital_species("pz1", l=1, atom='C')
+        .add_orbital_species("pz2", l=1, atom='C')
+        .add_orbital(position=(0, 0), tag="sublattice_1", species="pz1")
+        .add_orbital(position=(-1/3, -2/3), tag="sublattice_2", species="pz2")
+        .add_interaction(
+            "hamiltonian",
+            participants=("pz1", "pz2"),
+            parameters= [t1],
+        )
+        .add_interaction(
+            "hamiltonian",
+            participants=("pz1", "pz1"),            
+            # a bit overcomplicated
+            parameters=[                
+                [0, 0, 0, delta], # onsite                
+                # clockwise hoppings
+                [-2.46, 0, 0, t2], 
+                [2.46, 0, 0, jnp.conj(t2)],
+                [2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, t2],
+                [-2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)],
+                [2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, t2],
+                [-2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)]
+            ],
+        )
+        .add_interaction(
+            "hamiltonian",
+            participants=("pz2", "pz2"),
+            parameters=[                
+                [0, 0, 0, delta], # onsite                
+                # clockwise hoppings
+                [-2.46, 0, 0, jnp.conj(t2)], 
+                [2.46, 0, 0, t2],
+                [2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)],
+                [-2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, t2],
+                [2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)],
+                [-2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, t2]
+            ],
+        )
+        .add_interaction(                
+            "coulomb",
+            participants=("pz1", "pz2"),
+            parameters=[8.64],
+            expression=ohno_potential(0)
+        )
+        .add_interaction(
+            "coulomb",
+            participants=("pz1", "pz1"),
+            parameters=[16.522, 5.333],
+            expression=ohno_potential(0)
+        )
+        .add_interaction(
+            "coulomb",
+            participants=("pz2", "pz2"),
+            parameters=[16.522, 5.333],
+            expression=ohno_potential(0)
+        )
+    )
+
+### sim ###
+
+# MAYBE: conductivity from chi and RPA
+def rpa_conductivity(args_list):    
+    return
+
+def conductivity():
+    args_list = [
+        (Hexagon(40, armchair = True), -2.66, -1j*t2, 0.3, f"haldane_graphene_{t2}" )
+        for t2 in [0, 0.001, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, -0.03, -0.04, -0.05, -0.06]
+        ]
+    
+    res = {}    
+    omegas = jnp.linspace(0, 10, 100)    
+    for args in args_list:        
+        flake = get_haldane_graphene(*args[1:4]).cut_flake(args[0])
+        v = flake.velocity_operator
+        res[args[-1]] = jnp.array([[flake.get_ip_green_function(v[i], v[j], omegas) for i in range(2)] for j in range(2)])
+        
+    jnp.savez(f"lr_results.npz", **res)
+
+### postprocessing ###
+def plot_edge_states(args):
+    shape, t1, t2, delta, name = args
+    flake = get_haldane_graphene(t1, t2, delta).cut_flake(shape)    
+    idx = jnp.argwhere(jnp.abs(flake.energies) < 1e-1)[0].item()    
+    flake.show_2d(display = flake.eigenvectors[:, idx], scale = True, name = name)
+    
+def plot_energies(args):
+    shape, t1, t2, delta, name = args
+    flake = get_haldane_graphene(t1, t2, delta).cut_flake(shape)
+    flake.show_energies(name = name + "_energies.pdf")
+
+def plot_static():
+    plot_args_list = [
+        (Triangle(10, armchair = True), -2.66, -1j, 0.3, f"triangle" ),
+        (Hexagon(40, armchair = True), -2.66, -1j, 0.3, f"hexagon" ),
+        (Rectangle(40, 20, armchair = True), -2.66, -1j, 0.3, f"ribbon" ),        
+        ]    
+    for args in plot_args_list:
+        plot_edge_states(args)
+        plot_energies(args)
+
+# MAYBE: sum current from sites i->j going around the flake clock-wise
+def current_map(args_list):
+    return
+
+# TODO: lookup greens function
+def scattered_field(results_file, illu, r):
+    return
+
+# TODO:
+def average_chirality_density(sigma, illu):
+    return
+
+# TODO: turn matrix from cartesian to helicity basis        
+def to_helicity(mat):    
+    return
+    
+# TODO: load file, convert each matrix into helicity basis => compute difference between sigma_{++} and sigma_{--}
+def plot_chirality_difference(results_file):    
+    return    
+
+if __name__ == '__main__':
+    # haldane model has topological phase for Im[t2] > \frac{M}{3 \sqrt{3}} => for 0.3 Im[t_2]_crit ~ 0.06
+    # (shape, t1, t2, delta (mass term), name)
+    conductivity()
