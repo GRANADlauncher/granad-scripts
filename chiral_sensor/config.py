aa84c561fa42d787f4c99ad26e4582394cbd002a
r22dd <david.dams@tutanota.com>
2024-08-28 14:45:58 +0200

    FIX: typos

--- chiral_sensor/config.py
+++ chiral_sensor/config.py
@@ -6,52 +6,54 @@ from granad import *
 from lib import *
 
 IP_RESPONSE = True
-RPA_RESPONSE = True
-MEAN_FIELD = True
-GEOMETRIES = True
 PLOT_IP_RESPONSE = True
+LRT_FILE = 'lrt.npz'
+IP_ARGS = [ (Triangle(30, armchair = True), -2.66, -1j*t2, delta, f"haldane_graphene_{t2}" ) for (t2, delta) in [(0.0, 0.0), (0.1, 0.3), (0.5, 0.3)] ]
+
+RPA_RESPONSE = True
 PLOT_RPA_RESPONSE = True
+RPA_FILE = 'rpa_triangle.npz'
+RPA_FLAKE = get_haldane_graphene(-2.66, -0.5j, 0.3).cut_flake(Triangle(30))
+RPA_VALS = [0, 0.01, 0.1, 0.5, 0.7, 1.0]
+
+MEAN_FIELD = True
 PLOT_MEAN_FIELD = True
+MF_FLAKE = get_haldane_graphene(-2.66, -1j, 0.3).cut_flake(Triangle(30))
+MF_VALS = [0, 0.1, 0.2, 1., 1.5, 2., 2.5, 3.]
+MF_MIX, MF_ITER, MF_PREC = 0.0, 100, 1e-10
+MF_FILE = 'mf.npz'
+
 PLOT_GEOMETRIES = True
+GEOMETRIES = [
+    (shape, -2.66, -1j, 0.3, f"haldane_graphene" )
+    for shape in [Triangle(18, armchair = False), Rectangle(10, 10), Hexagon(20, armchair = True)]
+]
 
 if __name__ == '__main__':
     if IP_RESPONSE:
-        args_list = [
-            (Triangle(30, armchair = True), -2.66, -1j*t2, delta, f"haldane_graphene_{t2}" )
-            for (t2, delta) in [(0.0, 0.0), (0.1, 0.3), (0.5, 0.3)]
-        ]
-        ip_response(f, args_list)
+        ip_response(LRT_FILE, IP_ARGS)
         
     if PLOT_IP_RESPONSE:
-        plot_chirality_difference("cond_" + f)
-        plot_chirality("cond_" + f)
-        plot_power("cond_" + f)
-        plot_response_functions(f)
+        plot_chirality_difference("cond_" + LRT_FILE)
+        plot_chirality("cond_" + LRT_FILE)
+        plot_power("cond_" + LRT_FILE)
+        plot_response_functions(LRT_FILE)
 
     if RPA_RESPONSE:
-        flake = get_haldane_graphene(-2.66, -0.5j, 0.3).cut_flake(Triangle(30))
-        rpa_response(flake, "triangle", [0, 0.01, 0.1, 0.5, 0.7, 1.0])
+        rpa_response(RPA_FLAKE, RPA_FILE, RPA_VALS)
 
     if PLOT_RPA_RESPONSE:
-        plot_rpa_response("rpa_triangle.npz")
+        plot_rpa_response(RPA_FILE)
 
     if MEAN_FIELD:
-        t1, t2, delta, shape = -2.66, -1j, 0.3, Triangle(30)
-        flake = get_haldane_graphene(t1, t2, delta).cut_flake(shape)
-        Us = [0, 0.1, 0.2, 1., 1.5, 2., 2.5, 3.]
-        res = [scf_loop(flake, U, 0.0, 1e-10, 100) for U in Us]
-        jnp.savez("scf.npz", res = res, Us = Us, pos = flake.positions)
+        res = [scf_loop(MF_FLAKE, u, MF_MIX, MF_ITER, MF_PREC) for u in MF_VALS]
+        jnp.savez(MF_FILE, res = res, Us = MF_VALS, pos = MF_FLAKE.positions)
 
     if PLOT_MEAN_FIELD:
-        plot_stability("scf.npz")
+        plot_stability(MF_FILE)
 
     if PLOT_GEOMETRIES:
-        # plot edge states vs localization-annotated energy landscape of a few structures
-        setups = [
-            (shape, -2.66, -1j, 0.3, f"haldane_graphene" )
-            for shape in [Triangle(18, armchair = False), Rectangle(10, 10), Hexagon(20, armchair = True)]
-        ]
-        plot_edge_states_energy_landscape(setups)
-        plot_localization_varying_hopping(setups)
+        plot_edge_states_energy_landscape(GEOMETRIES)
+        plot_localization_varying_hopping(GEOMETRIES)
 
 
--- chiral_sensor/lib.py
+++ chiral_sensor/lib.py
@@ -32,37 +32,77 @@ def to_helicity(mat):
     trafo_inv = jnp.linalg.inv(trafo)
     return jnp.einsum('ij,jmk,ml->ilk', trafo, mat, trafo_inv)
 
+def get_haldane_graphene(t1, t2, delta):
+    """Constructs a graphene model with
+    onsite hopping difference between sublattice A and B, nn hopping, nnn hopping = delta, t1, t2
+    """
+    return (
+        Material("haldane_graphene")
+        .lattice_constant(2.46)
+        .lattice_basis([
+            [1, 0, 0],
+            [-0.5, jnp.sqrt(3)/2, 0]
+        ])
+        .add_orbital_species("pz1", l=1, atom='C')
+        .add_orbital_species("pz2", l=1, atom='C')
+        .add_orbital(position=(0, 0), tag="sublattice_1", species="pz1")
+        .add_orbital(position=(-1/3, -2/3), tag="sublattice_2", species="pz2")
+        .add_interaction(
+            "hamiltonian",
+            participants=("pz1", "pz2"),
+            parameters= [t1],
+        )
+        .add_interaction(
+            "hamiltonian",
+            participants=("pz1", "pz1"),            
+            # a bit overcomplicated
+            parameters=[                
+                [0, 0, 0, delta], # onsite                
+                # clockwise hoppings
+                [-2.46, 0, 0, t2], 
+                [2.46, 0, 0, jnp.conj(t2)],
+                [2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, t2],
+                [-2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)],
+                [2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, t2],
+                [-2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)]
+            ],
+        )
+        .add_interaction(
+            "hamiltonian",
+            participants=("pz2", "pz2"),
+            parameters=[                
+                [0, 0, 0, 0], # onsite                
+                # clockwise hoppings
+                [-2.46, 0, 0, jnp.conj(t2)], 
+                [2.46, 0, 0, t2],
+                [2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)],
+                [-2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, t2],
+                [2.46*0.5, -2.46*jnp.sqrt(3)/2, 0, jnp.conj(t2)],
+                [-2.46*0.5, 2.46*jnp.sqrt(3)/2, 0, t2]
+            ],
+        )
+        .add_interaction(                
+            "coulomb",
+            participants=("pz1", "pz2"),
+            parameters=[8.64],
+            expression=ohno_potential(0)
+        )
+        .add_interaction(
+            "coulomb",
+            participants=("pz1", "pz1"),
+            parameters=[16.522, 5.333],
+            expression=ohno_potential(0)
+        )
+        .add_interaction(
+            "coulomb",
+            participants=("pz2", "pz2"),
+            parameters=[16.522, 5.333],
+            expression=ohno_potential(0)
+        )
+    )
+
 
 ### RESPONSE ###
-def rpa_sus(evs, omegas, occupations, energies, coulomb, relaxation_rate = 1e-1):
-    """computes pp-response in RPA, following https://pubs.acs.org/doi/10.1021/nn204780e"""
-    
-    def inner(omega):
-        mat = delta_occ / (omega + delta_e + 1j*relaxation_rate)
-        sus = jnp.einsum('ab, ak, al, bk, bl -> kl', mat, evs, evs.conj(), evs.conj(), evs)
-        return sus @ jnp.linalg.inv(one - coulomb @ sus)
-
-    one = jnp.identity(evs.shape[0])
-    evs = evs.T
-    delta_occ = (occupations[:, None] - occupations) * flake.electrons
-    delta_e = energies[:, None] - energies
-    
-    return jax.lax.map(jax.jit(inner), omegas)
-
-def rpa_response(flake, results_file, cs):
-    """computes j-j response from p-p in RPA"""
-       
-    omegas =  jnp.linspace(0, 15, 150)
-    res = []
-    
-    for c in cs:        
-        sus = rpa_sus(flake.eigenvectors, omegas, flake.initial_density_matrix_e.diagonal(), flake.energies, c*flake.coulomb)        
-        p = flake.positions.T        
-        ref = jnp.einsum('Ii,wij,Jj->IJw', p, sus, p)        
-        res.append(omegas[None, None, :]**2 * ref)
-        
-    jnp.savez("rpa_" + results_file, cond = res, omegas = omegas, cs = cs)
-
 def ip_response(args_list, results_file):
     """computes IP j-j and p-p response. saves j-j, pp in "cond_" + results_file, "pol_" + results_file.
     """
@@ -261,7 +301,38 @@ def plot_power(results_file, keys = None):
     # Adjusting layout and saving
     plt.tight_layout()
     plt.savefig("power.pdf")
-    plt.close()    
+    plt.close()
+
+
+### RPA ###
+def rpa_sus(evs, omegas, occupations, energies, coulomb, relaxation_rate = 1e-1):
+    """computes pp-response in RPA, following https://pubs.acs.org/doi/10.1021/nn204780e"""
+    
+    def inner(omega):
+        mat = delta_occ / (omega + delta_e + 1j*relaxation_rate)
+        sus = jnp.einsum('ab, ak, al, bk, bl -> kl', mat, evs, evs.conj(), evs.conj(), evs)
+        return sus @ jnp.linalg.inv(one - coulomb @ sus)
+
+    one = jnp.identity(evs.shape[0])
+    evs = evs.T
+    delta_occ = (occupations[:, None] - occupations) * flake.electrons
+    delta_e = energies[:, None] - energies
+    
+    return jax.lax.map(jax.jit(inner), omegas)
+
+def rpa_response(flake, results_file, cs):
+    """computes j-j response from p-p in RPA"""
+       
+    omegas =  jnp.linspace(0, 15, 150)
+    res = []
+    
+    for c in cs:        
+        sus = rpa_sus(flake.eigenvectors, omegas, flake.initial_density_matrix_e.diagonal(), flake.energies, c*flake.coulomb)        
+        p = flake.positions.T        
+        ref = jnp.einsum('Ii,wij,Jj->IJw', p, sus, p)        
+        res.append(omegas[None, None, :]**2 * ref)
+        
+    jnp.savez(results_file, cond = res, omegas = omegas, cs = cs)
 
 def plot_rpa_response(results_file):
     with jnp.load(results_file) as data:
