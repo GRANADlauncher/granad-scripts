31489c1c164dd445d97d3069ff38c7335b562004
r22dd <david.dams@tutanota.com>
2025-06-02 10:01:23 +0200

    feat: ov working params

--- chain_ni/sim.py
+++ chain_ni/sim.py
@@ -16,7 +16,7 @@ def potential_s(r):
     # cutoff at 3.20 A
 
     q = 3.2
-    c = -2.319
+    c = -2.319*0.3
     r0 = 1.88
     
     r = jnp.linalg.norm(r)
@@ -33,7 +33,7 @@ def potential_p(r):
     # cutoff at 3.20 A
 
     q = 3.2
-    c = 1.306
+    c = 1.306*0.2
     r0 = 1.88
     
     r = jnp.linalg.norm(r)
@@ -66,14 +66,16 @@ if __name__ == '__main__':
     ni2 = get_ni(ni_orbs, 1)
 
     # carbon flake and ni atoms
-    flake = get_system([ni1, ni2], [[1,0,0], [-1.5, 0, 0]], size = 10)
+    flake = get_system([ni1, ni2], [[1 * jnp.cos(jnp.pi / 6),jnp.sin(jnp.pi / 6),0], [-1.5 * jnp.cos(jnp.pi / 6), 1.5*jnp.sin(jnp.pi / 6), 0]], size = 10)
+    #flake = get_system([ni1, ni2], [[1 * jnp.cos(jnp.pi / 6), 1 * jnp.sin(jnp.pi / 6),0], [-1.5*jnp.cos(jnp.pi / 6), 1.5*jnp.sin(jnp.pi / 6),0]], size = 7.4)
     flake.show_2d()
     
+    
     # coupling
-    flake.set_hamiltonian_groups("s", flake[0], potential_s)
+    flake.set_hamiltonian_groups("s", flake[0], potential_p)
 
-    flake.set_hamiltonian_element(ni1, ni1, 0j)
-    flake.set_hamiltonian_element(ni2, ni2, 1 + 0j)
+    flake.set_hamiltonian_element(ni1, ni1, 0 + 0j)
+    flake.set_hamiltonian_element(ni2, ni2, 0 + 0j)
 
     flake.show_2d(name = "7-acene.pdf")
     
@@ -82,6 +84,14 @@ if __name__ == '__main__':
     
     # eigenstate index most localized on Ni2
     level2 = int(jnp.argmax(jnp.abs(flake.eigenvectors[-1])))
+    print(flake.energies[level1], flake.energies[level2])
+    flake.set_excitation(level2, level1, 2)
+
+    flake.show_energies()
+    flake.show_2d(display = flake.eigenvectors[:, flake.homo])
+    flake.show_2d(display = flake.eigenvectors[:, level1])
+    flake.show_2d(display = flake.eigenvectors[:, level2])
+
 
     ## uncomment to visualize the dipole operator
     # plt.matshow(jnp.abs(flake.dipole_operator_e).sum(axis = 0)) # sum modulus of all cartesian components
@@ -96,31 +106,39 @@ if __name__ == '__main__':
     # flake.show_2d(display = flake.eigenvectors[:, level2])
 
     # cf. https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.109.267209, referenced by referee prb
-    freq = flake.energies[level1] - flake.energies[level2]
+    freq = jnp.abs(flake.energies[level2] - flake.energies[level1])
     print("Driving frequency = Transition frequency between localized states ", freq)
     
     pulse = Wave(
-        amplitudes=[1, 0, 0], frequency=freq,
+        amplitudes=[0.1, 0, 0], frequency=freq,
+    )
+
+    pulse = Pulse(
+        amplitudes=[0.1, 0, 0], frequency=freq, fwhm = 85, peak = 60,
     )
     
     result = flake.master_equation(
         grid = 10,
-        relaxation_rate = 1e-4,
+        relaxation_rate = 1/1000,
         illumination = pulse,
-        density_matrix = ["occ_x"], 
-        end_time = 80,
+        density_matrix = ["occ_e","occ_x"], 
+        end_time = 200,
         coulomb_strength = 0,
     )    
-    occ_x = result.output[0]
+    occ_e = result.output[0]
+    occ_x = result.output[1]
     
-    n_steady = 0
+    n_steady = jnp.argmin((occ_x[n_steady:, -2] - occ_x[n_steady:, -1]).real)
     
-    # real space occupation plot on Ni1, Ni2
-    plt.plot(result.time_axis[n_steady:], occ_x[n_steady:, -2], '-.')
-    plt.plot(result.time_axis[n_steady:], occ_x[n_steady:, -1])
+        # real space occupation plot on Ni1, Ni2
+    plt.plot(result.time_axis[:n_steady], occ_x[:n_steady, -2], '-.')
+    plt.plot(result.time_axis[:n_steady], occ_x[:n_steady, -1])
+    # plt.plot(result.time_axis[:n_steady], jnp.abs(occ_x[:n_steady, -2] - occ_x[:n_steady, -1]), '.')    
+
+    #plt.plot(result.time_axis[n_steady:], (flake.electrons-2)*(jnp.sum(occ_x[n_steady:, 0:-3],axis=1)/(flake.electrons-2)-1))
 
     # energy space plot of level 1 and level 2
-    # plt.plot(result.time_axis[n_steady:], occ_x[n_steady:, level1], '-.')
-    # plt.plot(result.time_axis[n_steady:], occ_x[n_steady:, level2])
+    #plt.plot(result.time_axis[n_steady:], occ_e[n_steady:, level1], '-.')
+    #plt.plot(result.time_axis[n_steady:], occ_e[n_steady:, level2])
     
     plt.show()
