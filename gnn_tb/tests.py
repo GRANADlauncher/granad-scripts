b59552d6f751916e954c3ae9c318ea8299ded629
r22dd <david.dams@tutanota.com>
2025-06-11 13:44:58 +0200

    test: vanilla fusion net

--- gnn_tb/tests.py
+++ gnn_tb/tests.py
@@ -6,6 +6,7 @@ from main import *
 # https://flax-linen.readthedocs.io/en/latest/api_reference/flax.linen/module.html#flax.linen.Module.tabulate
 # print(ggnn.tabulate(jax.random.key(0), jnp.ones((1, 28, 28, 1)), compute_flops=True, compute_vjp_flops=True))
 
+# TODO: adjust
 def test_ggnn():
     n_nodes = 4 # 4 atoms
     n_nodes_large = 100
@@ -31,3 +32,24 @@ def test_ggnn():
 
     assert output.shape == (n_batch, n_nodes, n_feats)
 
+
+def test_spectral_vanilla():
+    n_batch = 3
+    n_nodes = 4
+    min_cells = 1
+    max_cells = 100 # 400 atoms
+    
+    # Model    
+    rng = jax.random.PRNGKey(42)
+    
+    # batch
+    batch, rng = generate_batch(rng, min_cells, max_cells, n_nodes, n_batch)
+
+    # setup model
+    fusion = FusionMLP(1, 10, 20, 10, 20)
+    params = fusion.init(rng, batch["energies"], batch["cell_arr"])
+    output = fusion.apply(params, batch["energies"], batch["cell_arr"])
+
+    print("# params ", sum(x.size for x in jax.tree_leaves(params)))
+
+    assert output.shape == (n_batch,1)
