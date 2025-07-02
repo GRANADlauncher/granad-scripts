8ef1d701fd5724ef52318df3ce3a414ae8d262ed
r22dd <david.dams@tutanota.com>
2025-07-02 14:29:59 +0200

    test(gnn): cases for new funcs

--- gnn_tb/tests.py
+++ gnn_tb/tests.py
@@ -6,33 +6,21 @@ from main import *
 # https://flax-linen.readthedocs.io/en/latest/api_reference/flax.linen/module.html#flax.linen.Module.tabulate
 # print(ggnn.tabulate(jax.random.key(0), jnp.ones((1, 28, 28, 1)), compute_flops=True, compute_vjp_flops=True))
 
+
 def test_fusion():    
-    n_nodes = 4 # atoms
-    n_feats = 1
     n_batch = 3
-    n_hidden_dims = 4
-    n_dense_dim = 10
-    
-    min_cells = 4
-    max_cells = 5
+    max_atoms = 4
+    rng = jax.random.PRNGKey(42)
+    max_supercells = 3
 
-    # init random number generator
-    rng = jax.random.PRNGKey(0)
-
-    # batch
-    batch, rng = generate_batch(rng, min_cells, max_cells, n_nodes, n_batch)
-
-    stack = GGNNStack(n_nodes, n_feats, n_batch, n_hidden_dims, n_dense_dim, use_residual = False)
-
-    variables = stack.init(rng, batch, rng)
-    output = stack.apply(variables, batch, rng)
+    batch = generate_batch(n_batch, rng, max_atoms, max_supercells)
 
     spectral_config = {"n_hidden" : 8, "n_out" : 10}
-    ggnn_stack_config = {"n_nodes" : 4, "n_feats" : 1, "n_batch" : 3, "n_hidden_dims" : 4, "n_dense_dim" : 10, "use_residual" : False}
+    ggnn_stack_config = {"n_nodes" : max_atoms**2, "n_feats" : 1, "n_batch" : n_batch, "n_hidden_dims" : 4, "n_dense_dim" : 10, "use_residual" : False}
     cnn_config = {"features" : [32, 64], "kernels" : [(3,3), (3,3)], "windows" : [(2,2), (2,2)], "strides" : [(2,2), (2,2)], "n_hidden_features" : 256, "n_out_features" : 10}    
     fusion = FusionMLP(spectral_config, ggnn_stack_config, cnn_config, n_hidden = 10, n_out = 1)
-    variables = fusion.init(rng, jax.random.randint(rng, (n_batch, 10), 0, 2), batch, rng, jax.random.randint(rng, (n_batch, 10,10, 1), 0, 2))
-    output = fusion.apply(variables, jax.random.randint(rng, (n_batch, 10), 0, 2), batch, rng, jax.random.randint(rng, (n_batch, 10,10, 1), 0, 2))
+    variables = fusion.init(rng, batch, rng)
+    output = fusion.apply(variables, batch, rng)
     assert output.shape == (3,1)
 
 
@@ -56,40 +44,36 @@ def test_spectral():
     assert output.shape == (2, 10)
 
 def test_ggnn_stack():
-    n_nodes = 4 # atoms
+    n_batch = 4
+    max_atoms = 5
+    rng = jax.random.PRNGKey(42)
+    max_supercells = 3
     n_feats = 1
-    n_batch = 3
-    n_hidden_dims = (10, 4)
-    n_dense_dim = 10
+    n_hidden_dims = 8
+    n_dense_dim = 4
+    n_nodes = max_atoms**2
+
+    batch = generate_batch(n_batch, rng, max_atoms, max_supercells)
     
-    min_cells = 4
-    max_cells = 5
-
-    # init random number generator
-    rng = jax.random.PRNGKey(0)
-
-    # batch
-    batch, rng = generate_batch(rng, min_cells, max_cells, n_nodes, n_batch)
-
     stack = GGNNStack(n_nodes, n_feats, n_batch, n_hidden_dims, n_dense_dim, use_residual = False)
 
     variables = stack.init(rng, batch, rng)
     output = stack.apply(variables, batch, rng)
 
+    assert output.shape == (n_batch, n_nodes)
+
 
 def test_ggnn():
-    n_nodes = 4 # atoms
-    n_feats = 1
     n_batch = 3
+    max_atoms = 4
+    rng = jax.random.PRNGKey(42)
+    max_supercells = 3
+    n_feats = 1
+    n_hidden_dims = 8
+    n_dense_dim = 4
+    n_nodes = max_atoms**2
 
-    min_cells = 4
-    max_cells = 5
-
-    # init random number generator
-    rng = jax.random.PRNGKey(0)
-
-    # batch
-    batch, rng = generate_batch(rng, min_cells, max_cells, n_nodes, n_batch)
+    batch = generate_batch(n_batch, rng, max_atoms, max_supercells)
 
     # initialize single layer
     ggnn = GGNNLayer(n_nodes, n_feats, n_batch)
@@ -99,32 +83,6 @@ def test_ggnn():
     output, _ = ggnn.apply(variables, batch["node_features"], batch["hamiltonian"], rng, carry = None)
 
     assert output.shape == (n_batch, n_nodes, n_feats)
-
-
-def test_ggnn_stack():
-    n_nodes = 4 # atoms
-    n_feats = 1
-    n_batch = 3
-    n_hidden_dims = 10
-    n_dense_dim = 10
-    
-    min_cells = 4
-    max_cells = 5
-
-    # init random number generator
-    rng = jax.random.PRNGKey(0)
-
-    # batch
-    batch, rng = generate_batch(rng, min_cells, max_cells, n_nodes, n_batch)
-
-    stack = GGNNStack(n_nodes, n_feats, n_natch, n_hidden_dims, n_dense_dim, use_residual = False)
-
-    variables = stack.init(rng, batch, rng)
-    output = ggnn.apply(variables, batch, rng)
-
-    
-    
-    
 
 def test_spectral_vanilla():
     from vanilla import FusionMLP, generate_batch
