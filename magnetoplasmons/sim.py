c2df7c0283779c794e4f5cb06e6e4af531766132
r22dd <david.dams@tutanota.com>
2025-08-27 14:19:15 +0200

    feat: landau levels correct magnetic field

--- magnetoplasmons/sim.py
+++ magnetoplasmons/sim.py
@@ -2,10 +2,17 @@ import jax.numpy as jnp
 from granad import *
 from granad._plotting import *
 
-def peierls_coupling(B, t, r1, r2):
-    plus = r1 + r2
-    minus = r1 - r2
-    return complex(t * jnp.exp(1j * B * minus[0] * plus[1]))
+def get_peierls_coupling(B, t):
+
+    def inner(r1, r2):        
+        d = jnp.linalg.norm(r1 - r2)
+        return jax.lax.cond(jnp.abs(d - 1.42) < 1e-2,
+                            lambda x : t * jnp.exp(1j * B * (r1-r2)[0] * (r1+r2)[1]),
+                            lambda x : 0j,
+                            d
+                            )
+
+    return inner
 
 def get_graphene_b_field(B, t, shape):
     """obtain tb model for graphene in magnetic field from peierls substitution
@@ -15,40 +22,8 @@ def get_graphene_b_field(B, t, shape):
         t : nn hopping
     
     """
-    graphene_peierls = (
-        Material("graphene_peierls")
-        .lattice_constant(2.46)
-        .lattice_basis([
-            [1, 0, 0],
-            [-0.5, jnp.sqrt(3)/2, 0]
-        ])
-        .add_orbital_species("pz", atom='C')
-        .add_orbital(position=(0, 0), tag="sublattice_1", species="pz")
-        .add_orbital(position=(-1/3, -2/3), tag="sublattice_2", species="pz")
-        .add_interaction(
-            "hamiltonian",
-            participants=("pz", "pz"),
-            parameters = [0],
-        )
-        .add_interaction(
-            "coulomb",
-            participants=("pz", "pz"),
-            expression = ohno_potential(1.42)
-        )
-    )
-    
-    flake = graphene_peierls.cut_flake(shape)
-    distances = jnp.round(jnp.linalg.norm(flake.positions - flake.positions[:, None], axis = -1), 4)
-    nn = 1.5
-
-    # this is slightly awkward: our coupling only depends on the distance vector, but peierls substitution in landau
-    # gauge A = (0, Bx, 0) leads to peierls phase of \int_r1^r2 A dl = (y2 - y1) (x2 + x1) / 2
-    # we patch this manually here, but this is very, very slow
-    for i, orb1 in enumerate(flake):
-        for j in range(i+1):
-            if 0 < distances[i, j] <= nn:
-                orb2 = flake[j]
-                flake.set_hamiltonian_element(orb1, orb2, peierls_coupling(B, t, orb1.position, orb2.position))
+    flake = get_graphene().cut_flake(shape)
+    flake.set_hamiltonian_groups(flake[0], flake[0], get_peierls_coupling(B, t))
     return flake
 
 def get_absorption_rpa(flake):    
@@ -71,7 +46,8 @@ def sim():
     beta = 1 / (kb * 300)
 
     # magnetic field / scaled by e * (1e-4 eV)**2
-    Bs = jnp.linspace(0, 10, 20) * 200 * 0.3 * 1e-8
+    Bs = jnp.linspace(0, 10, 20)  / 33
+    Bs = [1/3]
 
     # frequency    
     omegas = jnp.linspace(0, 1, 100)
@@ -87,7 +63,9 @@ def sim():
         print(len(flake))
         flake.set_beta(beta)
         flake.show_2d(name = "flake.pdf")
-        flake.show_energies(name = "ip_energies.pdf")
+        flake.show_energies(name = "ip_energies.pdf", e_max = 0.2, e_min = -0.2)
+        import pdb; pdb.set_trace()
+
         corr = flake.get_ip_green_function(flake.dipole_operator_e[0], flake.dipole_operator_e[0], omegas)
         corr_map.append(corr)
 
